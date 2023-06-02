a7e31f191114d0786b0093bfbd9e02fee9e2d559
applepiecapital <tyler.yahnke@applepiecapital.com>
2023-06-02 10:19:18 -0400

    Removing redirect to HTTPS

--- application.py
+++ application.py
@@ -2,11 +2,6 @@ from website import create_app
 from flask import redirect, request
 
 application = create_app()
-@application.before_request
-def redirect_to_https():
-    if request.url.startswith('http://'):
-        url = request.url.replace('http://', 'https://', 1)
-        return redirect(url, code=301)
 
 
 if __name__ == '__main__':
