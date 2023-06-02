b8b9b0ded1004c9444d340dd4eaa562fe66dce90
applepiecapital <tyler.yahnke@applepiecapital.com>
2023-06-02 09:45:46 -0400

    Adding redirect to HTTPS

--- application.py
+++ application.py
@@ -1,6 +1,13 @@
 from website import create_app
+from flask import redirect, request
 
 application = create_app()
+@application.before_request
+def redirect_to_https():
+    if request.url.startswith('http://'):
+        url = request.url.replace('http://', 'https://', 1)
+        return redirect(url, code=301)
+
 
 if __name__ == '__main__':
     application.run(debug=True)
