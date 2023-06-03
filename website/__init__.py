cba071083a5e5086dcddbc35e86f1ed6fd51f8d6
applepiecapital <tyler.yahnke@applepiecapital.com>
2023-06-03 09:27:52 -0400

    Adding db

--- website/__init__.py
+++ website/__init__.py
@@ -9,13 +9,11 @@ import json
 
 db = SQLAlchemy()
 APC_DB = "APC_Database.db"
-#ABSOLUTE_PATH = "/Users/tyleryahnke/PycharmProjects/webapp/website"
 
 def create_app():
     application = Flask(__name__)
     #sslify = SSLify(application)
     application.config['SECRET_KEY'] = '54ge5rg4e4eshg4serthg4s5h4esr8t674'
-    #application.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{path.join(ABSOLUTE_PATH, APC_DB)}'
     application.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{APC_DB}'
     db.init_app(application)
 
