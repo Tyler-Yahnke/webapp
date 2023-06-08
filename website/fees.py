c1892664282b372f4c1d96c1d3bd7bfaf3a21ee7
applepiecapital <tyler.yahnke@applepiecapital.com>
2023-06-08 07:10:49 -0400

    Removed API from fees tab

--- website/fees.py
+++ website/fees.py
@@ -2,22 +2,14 @@ from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from . import db
 from .models import Discounts
-import pygsheets
-import pandas as pd
-import json
-from google.oauth2 import service_account
+
 
 fees = Blueprint('fees', __name__)
 
 @fees.route('/fees', methods=['GET', 'POST'])
 @login_required
 def fees_func():
-    data = {'Brand':[''],
-                       '0 - 750K':[''],
-                       '751K - 1.5M':[''],
-                       '1.5M+':['']
-                       }
-    discount_table = pd.DataFrame(data)
+
 
 
     if request.method == 'GET':
