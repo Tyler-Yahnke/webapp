1baa74b9f60713d985f6a3ebc3c5fb321d69bd8f
Gaurav Saini <gaurav.saini@trantorinc.com>
2023-07-26 14:54:52 +0530

    Added virtual environment and gunicorn

--- scripts/dependencies_install.sh
+++ scripts/dependencies_install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash --login
 cd /home/ubuntu/webapp
+python3 -m venv venv
 source venv/bin/activate
 pip3  install -r requirements.txt
+pip3  install gunicorn
 mkdir logs
 mkdir tmp
 mkdir tmp/pids
