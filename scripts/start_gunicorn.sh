b070d62c1572a6b71335c85d7578561d9cc2698b
Gaurav Saini <gaurav.saini@trantorinc.com>
2023-07-26 14:57:15 +0530

    Fixed start gunicorn command

--- scripts/start_gunicorn.sh
+++ scripts/start_gunicorn.sh
@@ -1,6 +1,5 @@
 #!/bin/bash --login
 cd /home/ubuntu/webapp
 source venv/bin/activate
-gunicorn -D --pid tmp/pids/server.pid --access-logfile logs/gunicorn.log --error-logfile logs/gunicorn.log -b 0.0.0.0:5000 
-application:application
+gunicorn -D --pid tmp/pids/server.pid --access-logfile logs/gunicorn.log --error-logfile logs/gunicorn.log -b 0.0.0.0:5000 application:application
 
