8ac1342402c2b3d43e2fef547b3e313ee8ac7012
applepiecapital <tyler.yahnke@applepiecapital.com>
2023-08-18 15:20:10 -0400

    Trying to fix Index pull

--- website/index_pull.py
+++ website/index_pull.py
@@ -1,6 +1,5 @@
 import pymysql
-from datetime import date
-from datetime import datetime
+from datetime import datetime, date
 from pandas.tseries.offsets import BDay
 import requests
 from flask_mail import Message
@@ -52,7 +51,7 @@ def index_rate_updates():
 
         swap_date = datetime.strptime(date_3year_str, '%Y-%m-%dT%H:%M:%S%z').date()
         swap_date_str = swap_date.strftime('%Y-%m-%d')
-        new_vals = [swap_date_str, "{:.2%}".format(swap_3year), "{:.2%}".format(swap_4year), "{:.2%}".format(swap_5year),datetime.datetime.today()]
+        new_vals = [swap_date_str, "{:.2%}".format(swap_3year), "{:.2%}".format(swap_4year), "{:.2%}".format(swap_5year),datetime.today()]
 
         sql = """
                 INSERT INTO ebdb.swap_rate (`Date`, `3Year`,`4Year`,`5Year`,Created_Date)
@@ -97,10 +96,10 @@ def index_rate_updates():
 
         prime_date = datetime.strptime(prime_rate_str, '%Y-%m-%dT%H:%M:%S%z').date()
         prime_date_str = prime_date.strftime('%Y-%m-%d')
-        new_prime_vals = [prime_date_str, "{:.2%}".format(prime_rate),datetime.datetime.today()]
+        new_prime_vals = [prime_date_str, "{:.2%}".format(prime_rate),datetime.today()]
 
         sql = """
-                    INSERT INTO ebdb.prime_rate (`Date`,`Rate`,'Created_Date')
+                    INSERT INTO ebdb.prime_rate (`Date`,`Rate`,Created_Date)
                     VALUES (%s, %s, %s)
                 """
 
