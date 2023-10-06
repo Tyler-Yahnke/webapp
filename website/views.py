f130ba21c2a61464f90a813a65d99fbc3f17f5e4
applepiecapital <tyler.yahnke@applepiecapital.com>
2023-10-05 21:34:43 -0400

    Updating to Swaps

--- website/views.py
+++ website/views.py
@@ -15,7 +15,7 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    global today_date_time, recommit, today, prime_rate, userselection_scooters,userselection_term_missing, userselection_bridge_loan,userselection_ratetype,userselection_term, userselection_fee,userselection_pricing, userselection_grade, userselection_dp, selected_date, down_payment_fee_dict
+    global today_date_time, recommit, today, rate_card_index, prime_rate, swap_rate, userselection_scooters,userselection_term_missing, userselection_bridge_loan,userselection_ratetype,userselection_term, userselection_fee,userselection_pricing, userselection_grade, userselection_dp, selected_date, down_payment_fee_dict
 
 
 
@@ -113,15 +113,16 @@ def home():
         down_payment_fee_dict = {'Y': 0.25, 'N': 0.00, 'NA': 0.00}
 
         prime_rate = PrimeRate.query.order_by(desc(PrimeRate.Date)).first()
+        swap_rate = SwapRate.query.order_by(desc(SwapRate.Date)).first().three_Year
         today_date_time = datetime.datetime.today()
         today = today_date_time.date()
-        all_prime = datetime.datetime(2023, 5, 4)
+        #all_prime = datetime.datetime(2023, 5, 4)
+        all_swap = datetime.datetime(2023, 10, 5)
 
         log_selections()
 
         if userselection_bridge_loan == 'Y':
             bridge_loan_func()
-
             results = bridge_results
             return render_template("home.html", user=current_user, results=results, rate_card_table=rate_card_table, previous_data=previous_data)
 
@@ -138,6 +139,7 @@ def home():
             if (today_date_time - selected_date).days > 120:
                 flash('Credit Officer Approval Date > 120 Days Ago, Current Month Rate Card Used', category='error')
                 current_credit_policy()
+                rate_card_index = SwapRate.query.order_by(desc(SwapRate.Date)).first().three_Year
                 rate_card()
                 results = current_credit_policy_results
                 rate_card_table = rate_card_df
@@ -147,15 +149,17 @@ def home():
                 flash('Can\'t select a future date', category='error')
                 return render_template("home.html", user=current_user, results=results, rate_card_table=rate_card_table,previous_data=previous_data)
 
-            elif selected_date <= all_prime and userselection_pricing=='Cash Flow':
-                # Previous credit policy can be removed after 9/2/2023
+            elif selected_date <= all_swap:
                 previous_credit_policy()
+                rate_card_index = PrimeRate.query.order_by(desc(PrimeRate.Date)).first().Rate
+                rate_card()
                 results = previous_credit_policy_results
                 rate_card_table = rate_card_df
                 return render_template("home.html", user=current_user, results=results, rate_card_table=rate_card_table, previous_data=previous_data)
 
             else:
                 current_credit_policy()
+                rate_card_index = SwapRate.query.order_by(desc(SwapRate.Date)).first().three_Year
                 rate_card()
                 results = current_credit_policy_results
                 rate_card_table = rate_card_df
@@ -163,6 +167,7 @@ def home():
 
         else:
             current_credit_policy()
+            rate_card_index = SwapRate.query.order_by(desc(SwapRate.Date)).first().three_Year
             rate_card()
             results = current_credit_policy_results
             rate_card_table = rate_card_df
@@ -224,9 +229,10 @@ def bridge_loan_func():
 def scooters_func():
     global scooters_results
 
-    scooters_date = datetime.datetime(2023, 4, 7)
     #cashflow scooters deals are now getting cashflow pricing. # Proforma still gets the 1.5%
     scooters_cash_flow = datetime.datetime(2023, 7, 8)
+    #Started using 3year swap 10/6
+    scooters_swaps = datetime.datetime(2023, 10, 5)
     selected_date = request.form.get('selected_date')
 
     if selected_date == '' or recommit =='N':
@@ -237,39 +243,49 @@ def scooters_func():
 
     if (today_date_time - selected_date).days > 120:
         if userselection_pricing =='Pro Forma':
-            temp_base_spread = 1.5
+            temp_base_spread = 5.6
+            index_rate = float(swap_rate)
+            index_rate_used = 'Swap - 3 Year'
             flash('Credit Officer Approval Date > 120 Days Ago, Current Month Rate Card Used', category='error')
+        else:
+            temp_base_spread = 4.95
+            index_rate = float(swap_rate)
+            index_rate_used = 'Swap - 3 Year'
+            flash('Credit Officer Approval Date > 120 Days Ago, Current Month Rate Card Used', category='error')
+
+    elif selected_date < scooters_cash_flow:
+        temp_base_spread = 1.5
+        index_rate = float(prime_rate.Rate)
+        index_rate_used = 'Prime'
+
+    elif selected_date >= scooters_cash_flow and selected_date <= scooters_swaps:
+        if userselection_pricing =='Pro Forma':
+            temp_base_spread = 1.5
+            index_rate = float(prime_rate.Rate)
+            index_rate_used = 'Prime'
         else:
             # Selecting rate card
             selected_spread = Spreads.query.filter(
-                and_(Spreads.Start <= today, Spreads.End >= today,
+                and_(Spreads.Start <= selected_date,
+                Spreads.End >= selected_date,
                      Spreads.APCGrade == userselection_grade,
                      Spreads.PricingBasis == userselection_pricing,
                      Spreads.RateType == userselection_ratetype
                      )
             ).first()
             temp_base_spread = float(getattr(selected_spread, userselection_term))
-            flash('Credit Officer Approval Date > 120 Days Ago, Current Month Rate Card Used', category='error')
-
-    elif selected_date < scooters_date:
-        temp_base_spread = .75
-
-    elif selected_date >scooters_date and selected_date < scooters_cash_flow:
-        temp_base_spread = 1.5
+            index_rate = float(prime_rate.Rate)
+            index_rate_used = 'Prime'
 
     else:
         if userselection_pricing =='Pro Forma':
-            temp_base_spread = 1.5
+            temp_base_spread = 5.6
+            index_rate = float(swap_rate)
+            index_rate_used = 'Swap - 3 Year'
         else:
-            # Selecting rate card
-            selected_spread = Spreads.query.filter(
-                and_(Spreads.Start <= today, Spreads.End >= today,
-                     Spreads.APCGrade == userselection_grade,
-                     Spreads.PricingBasis == userselection_pricing,
-                     Spreads.RateType == userselection_ratetype
-                     )
-            ).first()
-            temp_base_spread = float(getattr(selected_spread, userselection_term))
+            temp_base_spread = 4.95
+            index_rate = float(swap_rate)
+            index_rate_used = 'Swap - 3 Year'
 
     # selecting the embedded fee
     selected_fee = db.session.query(EmbeddedFee).filter(EmbeddedFee.Fee_Buy_Down == userselection_fee).first()
@@ -277,14 +293,14 @@ def scooters_func():
 
     base_spread = round(temp_base_spread + float(temp_embedded), 4)
 
-    final_spread = round(base_spread + float(prime_rate.Rate), 4)
+    final_spread = round(base_spread + index_rate, 4)
 
     scooters_results = {
-        'index_rate_used': 'Prime',
+        'index_rate_used': index_rate_used,
         'rate_card_used': 'Scooters Pricing',
         'final_rate': f"{final_spread: .2f}%",
         'spread_rate': f"{base_spread: .2f}%",
-        'index_rate': f"{prime_rate.Rate}%",
+        'index_rate': f"{index_rate}%",
         'index_rate_date': prime_rate.Date.strftime('%m/%d/%Y'),
         'rate_type': 'Fixed',
         'loan_buyer_spread': f"{temp_base_spread: .2f}%",
@@ -330,11 +346,57 @@ def current_credit_policy():
     base_spread = round(
         float(temp_base_spread) + float(temp_embedded) + float(down_payment_fee_dict[userselection_dp]), 4)
 
+
     # final interest rate
-    final_spread = round(base_spread + float(prime_rate.Rate), 4)
+    final_spread = round(base_spread + float(swap_rate), 4)
 
     #return results
     current_credit_policy_results = {
+        'index_rate_used': "Swap - 3 Year",
+        'rate_card_used': userselection_pricing,
+        'final_rate': f"{final_spread: .2f}%",
+        'spread_rate': f"{base_spread: .2f}%",
+        'index_rate': f"{swap_rate}%",
+        'index_rate_date': SwapRate.query.order_by(desc(SwapRate.Date)).first().Date.strftime('%m/%d/%Y'),
+        'rate_type': 'Fixed',
+        'loan_buyer_spread': f"{temp_base_spread: .2f}%",
+        'loan_buyer_fee': f"{temp_embedded}%",
+        'loan_buyer_dp': f"{down_payment_fee_dict[userselection_dp]: .2f}%"
+    }
+    return (current_credit_policy_results)
+
+def previous_credit_policy():
+    global previous_credit_policy_results
+
+    previous_credit_policy_results = {}
+
+
+    # Selecting rate card
+    selected_spread = Spreads.query.filter(
+        and_(
+            Spreads.Start <= selected_date,
+            Spreads.End >= selected_date,
+            Spreads.APCGrade == userselection_grade,
+            Spreads.PricingBasis == userselection_pricing,
+            Spreads.RateType == userselection_ratetype
+        )
+    ).first()
+    temp_base_spread = getattr(selected_spread, userselection_term)
+
+
+    # selecting the embedded fee
+    selected_fee = db.session.query(EmbeddedFee).filter(EmbeddedFee.Fee_Buy_Down == userselection_fee).first()
+    temp_embedded = getattr(selected_fee, userselection_term)
+
+    # base spread
+    base_spread = round(
+        float(temp_base_spread) + float(temp_embedded) + float(down_payment_fee_dict[userselection_dp]), 4)
+
+    # final interest rate
+    final_spread = round(base_spread + float(prime_rate.Rate), 4)
+
+    # return results
+    previous_credit_policy_results = {
         'index_rate_used': 'Prime',
         'rate_card_used': userselection_pricing,
         'final_rate': f"{final_spread: .2f}%",
@@ -346,7 +408,7 @@ def current_credit_policy():
         'loan_buyer_fee': f"{temp_embedded}%",
         'loan_buyer_dp': f"{down_payment_fee_dict[userselection_dp]: .2f}%"
     }
-    return (current_credit_policy_results)
+    return (previous_credit_policy_results)
 
 def rate_card():
     global rate_card_df
@@ -389,7 +451,7 @@ def rate_card():
             temp_val = round(
                 float(column) + float(temp_embedded_table) + float(down_payment_fee_dict[userselection_dp]), 4)
 
-            temp_final = round((temp_val + (float(prime_rate.Rate))), 2)
+            temp_final = round((temp_val + float(rate_card_index)), 2)
 
             new_row.append("{:.2f}".format(temp_final) + '%')
 
@@ -402,121 +464,3 @@ def rate_card():
     rate_card_df.insert(1, 'APC Grade', grade_list)
 
     return rate_card_df
-
-def previous_credit_policy():
-    global previous_credit_policy_results, rate_card_df
-    print('old process')
-
-    previous_credit_policy_results = {}
-
-    #dict to convert user term selection to DB model
-    swap_spread_dic = {'60/60': 'three_Year',
-                       '60/84': 'four_Year',
-                       '84/84': 'four_Year',
-                       '84/120': 'five_Year',
-                       '120/120': 'five_Year'}
-    selected_swap = swap_spread_dic.get(request.form.get('term'))
-
-
-    # Selecting rate card
-    selected_spread = Spreads.query.filter(
-        and_(
-            Spreads.Start <= selected_date,
-            Spreads.End >= selected_date,
-            Spreads.APCGrade == userselection_grade,
-            Spreads.PricingBasis == userselection_pricing,
-            Spreads.RateType == userselection_ratetype
-        )
-    ).first()
-    temp_base_spread = getattr(selected_spread, userselection_term)
-
-    # selecting the embedded fee
-    selected_fee = db.session.query(EmbeddedFee).filter(EmbeddedFee.Fee_Buy_Down == userselection_fee).first()
-    temp_embedded = getattr(selected_fee, userselection_term)
-
-    # swap rate
-    swap_rate = SwapRate.query.order_by(desc(SwapRate.Date)).first()
-    temp_swap = getattr(swap_rate, selected_swap)
-
-
-    swap_3year = float(getattr(swap_rate, 'three_Year'))
-    swap_4year = float(getattr(swap_rate, 'four_Year'))
-    swap_5year = float(getattr(swap_rate, 'five_Year'))
-
-    swap_spread_dic = {'60/60': swap_3year,
-                       '60/84': swap_4year,
-                       '84/84': swap_4year,
-                       '84/120': swap_5year,
-                       '120/120': swap_5year}
-
-
-    # base spread
-    base_spread = round(
-        float(temp_base_spread) + float(temp_embedded) + float(down_payment_fee_dict[userselection_dp]), 4)
-
-    #final interest rate
-    final_spread = round(float(base_spread) + float(temp_swap), 4)
-
-    # selecting rate card for the table
-    temp_table = db.session.query(Spreads).filter(
-        and_(
-            Spreads.Start <= selected_date,
-            Spreads.End >= selected_date,
-            Spreads.PricingBasis == userselection_pricing,
-            Spreads.RateType == userselection_ratetype
-        )
-    ).all()
-
-
-
-    # table creation
-    nice_term_vals = ['60/60', '60/84', '84/84', '84/120', '120/120']
-    # Mapping between attribute names and nice terms
-    term_map = {'term_60_60': '60/60', 'term_60_84': '60/84', 'term_84_84': '84/84', 'term_84_120': '84/120',
-                'term_120_120': '120/120'}
-    transformed_values = list()
-    for spread_object in temp_table:
-        new_row = list()
-        for attr, term_val in term_map.items():
-            # Access the term value using the getattr function since the column names are dynamic
-            column = getattr(spread_object, attr)
-            selected_fee_table = EmbeddedFee.query.filter_by(Fee_Buy_Down=userselection_fee).first()
-            temp_embedded_table = getattr(selected_fee_table, attr)
-
-            temp_val = round(
-                float(column) + float(temp_embedded_table) + float(down_payment_fee_dict[userselection_dp]), 4)
-
-            temp_final = round((temp_val + swap_spread_dic[term_val]), 2)
-
-            new_row.append("{:.2f}".format(temp_final) + '%')
-
-        transformed_values.append(new_row)
-
-    month_list = [spread_object.Month for spread_object in temp_table]
-    grade_list = [spread_object.APCGrade for spread_object in temp_table]
-    rate_card_df = pd.DataFrame(transformed_values, columns=nice_term_vals)
-    rate_card_df.insert(0, 'Month', month_list)
-    rate_card_df.insert(1, 'APC Grade', grade_list)
-
-    swap_spread_dic_results = {'60/60': '3 Year',
-                       '60/84': '4 Year',
-                       '84/84': '4 Year',
-                       '84/120': '5 Year',
-                       '120/120': '5 Year'}
-    selected_swap_results = swap_spread_dic_results.get(request.form.get('term'))
-
-    previous_credit_policy_results = {
-        'index_rate_used': f"Swap - {selected_swap_results}",
-        'rate_card_used': userselection_pricing,
-        'final_rate': f"{final_spread: .2f}%",
-        'spread_rate': f"{base_spread: .2f}%",
-        'index_rate': f"{temp_swap}%",
-        'index_rate_date': swap_rate.Date.strftime('%m/%d/%Y'),
-        'rate_type': 'Fixed',
-        'loan_buyer_spread': f"{temp_base_spread: .2f}%",
-        'loan_buyer_fee': f"{temp_embedded}%",
-        'loan_buyer_dp': f"{down_payment_fee_dict[userselection_dp]: .2f}%"
-    }
-    return (previous_credit_policy_results, rate_card_df)
-
-
