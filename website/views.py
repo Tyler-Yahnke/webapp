21b5fc62e4ea1c24d101bf81acad6e4d3b5a5e71
applepiecapital <tyler.yahnke@applepiecapital.com>
2025-01-08 07:25:53 -0500

    Fixing spread rate not updated logic and func rate change message

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 import datetime
-from datetime import date
+from datetime import date, timedelta
 from pandas.tseries.offsets import BDay
 import requests
 from .models import SwapRate, PrimeRate, Spreads, EmbeddedFee, RateCard
@@ -127,8 +127,6 @@ def home():
             # Handle the case when no date is selected. Set selected_date to today's date
             selected_date = datetime.datetime.today().date()
 
-
-
         try:
             # Pulling most recent spread to ensure it has been updated
             selected_spread = Spreads.query.filter(
@@ -140,7 +138,12 @@ def home():
                     Spreads.RateType == userselection_ratetype
                 )
             ).order_by(Spreads.End.desc()).first()
-
+            if selected_spread == None:
+                flash('Spread Rates have not been updated. Please notify Tyler Yahnke', category='error')
+                return render_template("home.html", user=current_user, results=results, rate_card_table=rate_card_table,
+                                       previous_data=previous_data)
+            else:
+                pass
 
         except:
             flash('Spread Rates have not been updated. Please notify Tyler Yahnke', category='error')
@@ -155,24 +158,6 @@ def home():
             bridge_loan_func()
             results = bridge_results
             log_selections()
-            return render_template("home.html", user=current_user, results=results, rate_card_table=rate_card_table, previous_data=previous_data)
-
-        if userselection_brand == 'Scooters':
-
-            #Date Scooters Stopped having special pricing
-            scooters_end_date = datetime.datetime(2024, 5, 20)
-            if recommit =='Y' and selected_date < scooters_end_date and (today_date_time - selected_date).days < 120:
-                scooters_func()
-                results = scooters_results
-                log_selections()
-            else:
-                current_credit_policy()
-                rate_card_index = SwapRate.query.order_by(desc(SwapRate.Date)).first().three_Year
-                rate_card()
-                results = current_credit_policy_results
-                rate_card_table = rate_card_df
-                log_selections()
-
             return render_template("home.html", user=current_user, results=results, rate_card_table=rate_card_table, previous_data=previous_data)
 
 
@@ -236,8 +221,6 @@ def missing():
 
 def log_selections():
     print('logging results')
-    # Date Scooters Stopped having special pricing
-    scooters_end_date = datetime.datetime(2024, 5, 20)
 
     if userselection_bridge_loan=='Y':
         rate_card = RateCard(
@@ -261,33 +244,6 @@ def log_selections():
             down_payment_penalty = '0.00',
             brand_penalty=bridge_results['loan_buyer_brand_fee'].strip('%'),
             embedded_fee_penalty = '0.00'
-        )
-        db.session.add(rate_card)
-        db.session.commit()
-        print('logged_successfully')
-
-    elif userselection_brand == 'Scooters' and recommit =='Y' and selected_date < scooters_end_date and (today_date_time - selected_date).days < 120:
-        rate_card = RateCard(
-            user=current_user.name,
-            calculated_date=datetime.datetime.today(),
-            recommit=recommit,
-            credit_officer_approval_date=selected_date,
-            scooters='Y',
-            brand=userselection_brand,
-            bawag=userselection_bawag_loan,
-            bridge=userselection_bridge_loan,
-            rate_type=userselection_ratetype ,
-            term=userselection_term,
-            embedded_fee=userselection_fee ,
-            investment_grade = userselection_grade,
-            pricing_basis=userselection_pricing ,
-            down_payment = userselection_dp,
-            interest_rate = scooters_results['final_rate'].strip('%'),
-            spread_rate = scooters_results['spread_rate'].strip('%'),
-            index_rate = scooters_results['index_rate'].strip('%'),
-            down_payment_penalty = '0.00',
-            brand_penalty=scooters_results['loan_buyer_brand_fee'].strip('%'),
-            embedded_fee_penalty = scooters_results['loan_buyer_fee'].strip('%')
         )
         db.session.add(rate_card)
         db.session.commit()
@@ -339,60 +295,6 @@ def bridge_loan_func():
     'rate_type': 'Fixed'}
     return(bridge_results)
 
-def scooters_func():
-    global scooters_results
-
-    selected_date = request.form.get('selected_date')
-
-    if selected_date == '' or recommit =='N':
-        selected_date = datetime.datetime.today()
-    else:
-        selected_date = datetime.datetime.strptime(selected_date, "%Y-%m-%d")
-
-
-    if (today_date_time - selected_date).days > 120:
-        if userselection_pricing =='Pro Forma':
-            temp_base_spread = 5.6
-            index_rate = float(swap_rate)
-            index_rate_used = 'Swap - 3 Year'
-            flash('Credit Officer Approval Date > 120 Days Ago, Current Month Rate Card Used', category='error')
-        else:
-            temp_base_spread = 4.95
-            index_rate = float(swap_rate)
-            index_rate_used = 'Swap - 3 Year'
-            flash('Credit Officer Approval Date > 120 Days Ago, Current Month Rate Card Used', category='error')
-
-    else:
-        if userselection_pricing =='Pro Forma':
-            temp_base_spread = 5.6
-            index_rate = float(swap_rate)
-            index_rate_used = 'Swap - 3 Year'
-        else:
-            temp_base_spread = 4.95
-            index_rate = float(swap_rate)
-            index_rate_used = 'Swap - 3 Year'
-
-    # selecting the embedded fee
-    selected_fee = db.session.query(EmbeddedFee).filter(EmbeddedFee.Fee_Buy_Down == userselection_fee).first()
-    temp_embedded = getattr(selected_fee, userselection_term)
-
-    base_spread = round(temp_base_spread + float(temp_embedded), 4)
-
-    final_spread = round(base_spread + index_rate, 4)
-
-    scooters_results = {
-        'index_rate_used': index_rate_used,
-        'rate_card_used': 'Scooters Pricing',
-        'final_rate': f"{final_spread: .2f}%",
-        'spread_rate': f"{base_spread: .2f}%",
-        'index_rate': f"{index_rate}%",
-        'index_rate_date': prime_rate.Date.strftime('%m/%d/%Y'),
-        'rate_type': 'Fixed',
-        'loan_buyer_spread': f"{temp_base_spread: .2f}%",
-        'loan_buyer_fee': f"{temp_embedded}%",
-        'loan_buyer_brand_fee': "0.00%"
-    }
-    return (scooters_results)
 
 def current_credit_policy():
     print('new process')
@@ -423,6 +325,9 @@ def current_credit_policy():
             )
         ).first()
         temp_base_spread = getattr(selected_spread, userselection_term)
+
+        rate_change_message()
+
 
     # selecting the embedded fee
     selected_fee = db.session.query(EmbeddedFee).filter(EmbeddedFee.Fee_Buy_Down == userselection_fee).first()
@@ -506,3 +411,75 @@ def rate_card():
     rate_card_df.insert(1, 'APC Grade', grade_list)
 
     return rate_card_df
+
+def rate_change_message():
+    #selecting current pro forma rate
+    selected_spread_pro_forma = Spreads.query.filter(
+        and_(Spreads.Start <= today, Spreads.End >= today,
+             Spreads.APCGrade == userselection_grade,
+             Spreads.PricingBasis == 'Pro Forma',
+             Spreads.RateType == userselection_ratetype
+             )
+    ).first()
+    temp_base_spread_pro_forma = getattr(selected_spread_pro_forma, userselection_term)
+
+
+
+
+    #selecting current cash flow rate
+    selected_spread_cash_flow = Spreads.query.filter(
+        and_(Spreads.Start <= today, Spreads.End >= today,
+             Spreads.APCGrade == userselection_grade,
+             Spreads.PricingBasis == 'Cash Flow',
+             Spreads.RateType == userselection_ratetype
+             )
+    ).first()
+    temp_base_spread_cash_flow = getattr(selected_spread_cash_flow, userselection_term)
+
+    # Displaying change in rate from previous month rate card for users
+    spread_end_date = (selected_spread_pro_forma.End).strftime('%m-%d-%Y')
+    spread_start_date = (selected_spread_pro_forma.Start).strftime('%m-%d-%Y')
+
+    previous_spread_start_date = (selected_spread_pro_forma.Start - timedelta(days=1))
+
+    # Prior month Pro Forma Spread Rate
+    selected_spread_previous_month = Spreads.query.filter(
+        and_(Spreads.Start <= previous_spread_start_date, Spreads.End >= previous_spread_start_date,
+             Spreads.APCGrade == userselection_grade,
+             Spreads.PricingBasis == 'Pro Forma',
+             Spreads.RateType == userselection_ratetype
+             )
+    ).first()
+    previous_month_pro_forma = getattr(selected_spread_previous_month, userselection_term)
+
+    #Prior month Cash Flow Spread Rate
+    selected_spread_previous_month = Spreads.query.filter(
+        and_(Spreads.Start <= previous_spread_start_date, Spreads.End >= previous_spread_start_date,
+             Spreads.APCGrade == userselection_grade,
+             Spreads.PricingBasis == 'Cash Flow',
+             Spreads.RateType == userselection_ratetype
+             )
+    ).first()
+    previous_month_cash_flow = getattr(selected_spread_previous_month, userselection_term)
+
+    pro_forma_rate_change = f"{(temp_base_spread_pro_forma - previous_month_pro_forma)}%"
+    cash_flow_rate_change = f"{(temp_base_spread_cash_flow - previous_month_cash_flow)}%"
+
+    if temp_base_spread_pro_forma - previous_month_pro_forma > 0:
+        pro_forma_message = f"Pro Forma Rates have increased {pro_forma_rate_change}"
+    elif temp_base_spread_pro_forma - previous_month_pro_forma < 0:
+        pro_forma_message = f"Pro Forma Rates have decreased {pro_forma_rate_change}"
+    else:
+        pro_forma_message = "Pro Forma Rates are unchanged from previous month"
+
+    if temp_base_spread_cash_flow - previous_month_cash_flow > 0:
+        cash_flow_message = f"Cash Flow Rates have increased {cash_flow_rate_change}"
+    elif temp_base_spread_cash_flow - previous_month_cash_flow < 0:
+        cash_flow_message = f"Cash Flow Rates have decreased {cash_flow_rate_change}"
+    else:
+        cash_flow_message = "Cash Flow Rates are unchanged from previous month"
+
+    flash(
+        f"{pro_forma_message} and {cash_flow_message} as of {spread_start_date}. Current Rate Card Expires {spread_end_date}.",
+        category='error'
+)
